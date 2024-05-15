7b9754a208cd662aed50efd32a8a8e738ea7addc
André Escobar <andre.escobar@blintz.com.br>
2024-05-15 01:14:57 -0300

    feat: add webpack to build

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import nodeExternals from 'webpack-node-externals';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const config = {
+  mode: 'production',
+  entry: './index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'final.js',
+  },
+  target: 'node',
+  externals: [nodeExternals()],
+};
+
+export default config;
